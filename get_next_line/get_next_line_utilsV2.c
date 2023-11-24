18b6413ce478536cef70fe615a808466866f9346
1McLongLong <85419730+1McLongLong@users.noreply.github.com>
2023-11-24 12:55:58 +0100

    Update and rename get_next_line_utils.c to get_next_line_utilsV2.c

--- get_next_line/get_next_line_utilsV2.c
+++ get_next_line/get_next_line_utilsV2.c
@@ -1,17 +1,5 @@
 #include "get_next_line.h"
-/*
-size_t ft_strlen(char *str)
-{
-  size_t i;
 
-  if (str == NULL)
-    return (0);
-  i = 0;
-  while (str[i])
-    i++;
-  return (i);
-}
-*/
 
 size_t	ft_strlen(char *str)
 {
